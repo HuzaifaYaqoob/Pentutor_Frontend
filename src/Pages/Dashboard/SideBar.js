3aacf4470d8139f33f01cea3870a5e4db08c09eb
unknown <zeevhyorg@gmail.com>
2021-12-23 05:22:40 +0500

    Profile and dashboard changes

--- src/Pages/Dashboard/SideBar.js
+++ src/Pages/Dashboard/SideBar.js
@@ -9,22 +9,46 @@ import { HamburgerIcon } from '../../components/Header/Header'
 import { connect } from 'react-redux'
 
 import { apiBaseURL } from '../../redux/apiURLs'
+import { LoggoutUser } from '../../redux/Actions/UserActions/UserActions'
+import Cookies from 'js-cookie'
 
 const BarLink = (props) => {
     return (
-        <Link className={`flex items-center relative gap-4 py-2 rounded-lg px-3 mb-1 cursor-pointer  text-xs ` + (props.active ? 'bg-gray-300' : 'hover:bg-gray-200')} to={props.nextPath} >
-            <span className='w-5 h-5 flex items-center justify-center'>
-                <FontAwesomeIcon className='text-indigo-900' icon={props.icon} />
-            </span>
-            <p className='text-indigo-900 font-semibold capitalize text-xs'>{props.text}</p>
+        <>
             {
-                props.active ?
+                props.nextPath ?
+                    <Link className={`flex items-center relative gap-4 py-2 rounded-lg px-3 mb-1 cursor-pointer  text-xs ` + (props.active ? 'bg-gray-300' : 'hover:bg-gray-200')} to={props.nextPath} >
+                        <span className='w-5 h-5 flex items-center justify-center'>
+                            <FontAwesomeIcon className='text-indigo-900' icon={props.icon} />
+                        </span>
+                        <p className='text-indigo-900 font-semibold capitalize text-xs'>{props.text}</p>
+                        {
+                            props.active ?
 
-                    <FontAwesomeIcon className='text-indigo-900 absolute top-1/2 right-3 transform -translate-y-1/2' icon={faChevronRight} />
+                                <FontAwesomeIcon className='text-indigo-900 absolute top-1/2 right-3 transform -translate-y-1/2' icon={faChevronRight} />
+                                :
+                                <></>
+                        }
+                    </Link>
                     :
-                    <></>
+                    <div
+                        className={`flex items-center relative gap-4 py-2 rounded-lg px-3 mb-1 cursor-pointer  text-xs ` + (props.active ? 'bg-gray-300' : 'hover:bg-gray-200')}
+                        onClick={props.onClick && props.onClick}
+                    >
+                        <span className='w-5 h-5 flex items-center justify-center'>
+                            <FontAwesomeIcon className='text-indigo-900' icon={props.icon} />
+                        </span>
+                        <p className='text-indigo-900 font-semibold capitalize text-xs'>{props.text}</p>
+                        {
+                            props.active ?
+
+                                <FontAwesomeIcon className='text-indigo-900 absolute top-1/2 right-3 transform -translate-y-1/2' icon={faChevronRight} />
+                                :
+                                <></>
+                        }
+                    </div>
             }
-        </Link>
+        </>
     )
 }
 
@@ -80,9 +104,16 @@ const SideBar = (props) => {
                     <BarLink icon={faCreditCard} text='payment record' />
                     <BarLink icon={faHandshake} text='agreement' />
                     <BarLink icon={faThList} text='home tution' />
-                    <BarLink icon={faExclamationCircle} text='report' />
-                    <BarLink icon={faUser} text='Chat Box' nextPath='/chat/' />
-                    <BarLink icon={faUser} text='log out' />
+                    {/* <BarLink icon={faExclamationCircle} text='report' /> */}
+                    {/* <BarLink icon={faUser} text='Chat Box' nextPath='/chat/' /> */}
+                    <BarLink
+                        icon={faUser}
+                        text='log out'
+                        onClick={(e) => {
+                            Cookies.remove('auth_token')
+                            props.LoggoutUser()
+                        }}
+                    />
                 </ul>
             </div>
         </>
@@ -93,5 +124,8 @@ const mapStateToProps = state => {
     return state
 }
 
+const mapDispatchToProps = {
+    LoggoutUser: LoggoutUser
+}
 
-export default connect(mapStateToProps, null)(SideBar)
+export default connect(mapStateToProps, mapDispatchToProps)(SideBar)
