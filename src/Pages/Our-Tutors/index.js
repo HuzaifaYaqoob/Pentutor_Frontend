95766f80a87c07b0aebd7ae21c3ae89dfb7ee283
danishyaqoobofficial <danishyaqoob117@gmail.com>
2025-02-08 17:33:37 +0500

    djbvfe

--- src/Pages/Our-Tutors/index.js
+++ src/Pages/Our-Tutors/index.js
@@ -25,8 +25,8 @@ export const TutorCard = ({ data, ...props }) => {
 
     console.log(data)
     return (
-        <div className='rounded-xl p-7 mb-16 pt-24 relative w-full border !z-0'>
-            <div className='absolute w-36 h-36 shadow-xl flex items-center bg-white justify-center top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-full overflow-hidden !z-40'>
+        <div className='rounded-xl p-7 mb-16 pt-24 relative w-full border border-[#CACBE6] !z-0'>
+            <div className='absolute w-36 h-36 border border-[#CACBE6] flex items-center bg-white justify-center top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-full overflow-hidden !z-40'>
             <div
                 className="w-36 h-36 rounded-full bg-center bg-cover bg-no-repeat flex items-center justify-center"
                 style={data?.profile_image ? { backgroundImage: `url('${data.profile_image}')` } : {}}
